45b776985bb23c5684924fca37ac83dbe815ca7a
MAC <miguelangelcabrera1@hotmail.com>
2019-10-27 16:04:42 +0100

    Added a encoded string class

---
+++ src/algorithms/LZSS/encodedString.java
@@ -0,0 +1,12 @@
+package algorithms.LZSS;
+
+/**
+ * Class representing an encoded string
+ *
+ */
+public class encodedString {
+
+    int offset; //offset to start the longest match
+    int length; //length of the longest match
+    char c; //letter
+}
