8f6e97568de50a02eeb56a3420d5f75692a4bcbb
Victor <victorblancogarcia3@gmail.com>
2019-11-17 16:29:32 +0100

    Transaction drivers

---
+++ src/dominio/drivers/UnzipDriver.java
@@ -0,0 +1,46 @@
+package dominio.drivers;
+
+import dominio.Unzip;
+import dominio.model.ItemC;
+import java.io.File;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Scanner;
+
+public class UnzipDriver {
+
+  private Scanner scanner = new Scanner(System.in);
+  private Scanner scanner2 = new Scanner(System.in);
+
+  public static void main(String[] args) {
+    new UnzipDriver();
+  }
+
+  private UnzipDriver() {
+    System.out.println("Welcome to the driver of the Unzip transaction");
+    start();
+  }
+
+  private void start() {
+    System.out.println("Path: ");
+    String path = scanner.nextLine();
+    try {
+      new Unzip(new ItemC(new File(path))).execute();
+      System.out.println("File successfully decompressed.");
+      end();
+    } catch (UnsupportedEncodingException uee) {
+      System.out.println("Please, provide a supported extension (sippy)");
+      start();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private void end() {
+    System.out.println("\n\n");
+    System.out.println("Select option:\n\t1. Start over\n\t2. Exit");
+    if (scanner2.nextInt() == 1) {
+      start();
+    }
+  }
+}
