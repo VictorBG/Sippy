6221cdccfee81448ea89ece027900080d1efbdf1
YaizaCano <yaiza.cano@est.fib.upc.edu>
2019-11-17 09:50:32 +0100

    v true imports fixed

--- src/domain/algorithms/jpeg/JPEG.java
+++ src/domain/algorithms/jpeg/JPEG.java
@@ -7,7 +7,11 @@ import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
 
 import static java.lang.Math.abs;
 
