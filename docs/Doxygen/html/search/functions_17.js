c0ae30902790e62942c403833be493b40089aa7e
Victor <victorblancogarcia3@gmail.com>
2019-12-20 15:57:21 +0100

    more docu

---
+++ docs/Doxygen/html/search/functions_17.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['zigzag_798',['zigzag',['../classprop_1_1algorithms_1_1jpeg_1_1_j_p_e_g.html#a4558fb9b2c28269149b73ef1e1a54636',1,'prop::algorithms::jpeg::JPEG']]],
+  ['zipdriver_799',['ZipDriver',['../classprop_1_1dominio_1_1drivers_1_1_zip_driver.html#ab4a935ca3c642e853501cf919c98f65a',1,'prop::dominio::drivers::ZipDriver']]],
+  ['zipstreamimpl_800',['ZipStreamImpl',['../classprop_1_1datos_1_1streams_1_1impl_1_1_zip_stream_impl.html#a24e8e36f2519d0ef488ac84e3a2df6f2',1,'prop::datos::streams::impl::ZipStreamImpl']]],
+  ['ziptransaction_801',['ZipTransaction',['../classprop_1_1dominio_1_1_zip_transaction.html#a79080d72d403460df7a757e02ba05a83',1,'prop::dominio::ZipTransaction']]]
+];
