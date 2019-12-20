0e41f4abbc6484c10540cd7a18cdf90be6300e72
MAC <miguelangelcabrera1@hotmail.com>
2019-12-20 14:14:51 +0100

    Fixing tests... Still KO differ of 1 byte?

--- src/test/java/prop/algorithms/LZSSTest.java
+++ src/test/java/prop/algorithms/LZSSTest.java
@@ -12,14 +12,15 @@ public class LZSSTest extends BaseAlgorithmTest {
 
   @Test
   public void testLZSS() {
-    test(Algorithm.LZSS.getAlgorithm());
+    testEncode(Algorithm.LZSS.getAlgorithm());
+    testDecode(Algorithm.LZSS.getAlgorithm());
   }
 
   @Override
   public List<byte[]> getInputs() {
     return new ArrayList<byte[]>() {
       {
-        add("Lorem ipsum dolor sit amet.\n".getBytes());
+        add("Lorem ipsum dolor sit amet.".getBytes());
       }
     };
   }
@@ -28,13 +29,13 @@ public class LZSSTest extends BaseAlgorithmTest {
   public List<byte[]> getOutputs() {
     return new ArrayList<byte[]>() {
       {
-        add(new byte[]{                                       //f6
-                        (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x36, (byte) 0x00, (byte) 0x00,
-                        (byte) 0x00, (byte) 0x08, (byte) 0x4c, (byte) 0x6f, (byte) 0x72, (byte) 0x65,
-                        (byte) 0x6d, (byte) 0x20, (byte) 0x69, (byte) 0x70, (byte) 0x73, (byte) 0x75,
-                        (byte) 0x6d, (byte) 0x20, (byte) 0x64, (byte) 0x6f, (byte) 0x6c, (byte) 0x6f,
-                        (byte) 0x72, (byte) 0x20, (byte) 0x73, (byte) 0x69, (byte) 0x74, (byte) 0x20,
-                        (byte) 0x61, (byte) 0x6d, (byte) 0x65, (byte) 0x74, (byte) 0x2e
+        add(new byte[]{
+
+                        (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x1B, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x08,
+                        (byte) 0x4c, (byte) 0x6f, (byte) 0x72, (byte) 0x65, (byte) 0x6d, (byte) 0x20, (byte) 0x69, (byte) 0x70,
+                        (byte) 0x73, (byte) 0x75, (byte) 0x6d, (byte) 0x20, (byte) 0x64, (byte) 0x6f, (byte) 0x6c, (byte) 0x6f,
+                        (byte) 0x72, (byte) 0x20, (byte) 0x73, (byte) 0x69, (byte) 0x74, (byte) 0x20, (byte) 0x61, (byte) 0x6d,
+                        (byte) 0x65, (byte) 0x74, (byte) 0x2e
                 }
         );
       }
